8d052ad749ca0692c4fa8bcdba11d700192d5946
Yeshua0709 <abrenica.y@gmail.com>
2023-04-11 01:27:23 +0800

    Update sql schema

--- database/pms_db.sql
+++ database/pms_db.sql
@@ -1,19 +1,102 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2022 at 05:22 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.5
+-- Generation Time: Apr 10, 2023 at 07:26 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `pms_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `id` int(16) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `contactnumber` varchar(16) NOT NULL,
+  `reason` varchar(50) NOT NULL,
+  `status` varchar(16) NOT NULL,
+  `date` date NOT NULL,
+  `time` time(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`id`, `name`, `contactnumber`, `reason`, `status`, `date`, `time`) VALUES
+(9, 'Yeshua Miguel B. Abrenica', '09972157320', 'Pasta', 'Active', '2023-04-08', '09:00:00.000000'),
+(10, 'Juan Miguel Caparas', '09972157320', 'Xray', 'Active', '2023-04-10', '10:42:00.000000'),
+(11, 'Deadpool', '09972157320', 'Check up', 'Active', '2023-04-20', '17:43:00.000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dentalchart`
+--
+
+CREATE TABLE `dentalchart` (
+  `id` int(16) NOT NULL,
+  `patient_id` int(16) NOT NULL,
+  `t1` varchar(16) NOT NULL,
+  `t2` varchar(16) NOT NULL,
+  `t3` varchar(16) NOT NULL,
+  `t4` varchar(16) NOT NULL,
+  `t5` varchar(16) NOT NULL,
+  `t6` varchar(16) NOT NULL,
+  `t7` varchar(16) NOT NULL,
+  `t8` varchar(16) NOT NULL,
+  `t9` varchar(16) NOT NULL,
+  `t10` varchar(16) NOT NULL,
+  `t11` varchar(16) NOT NULL,
+  `t12` varchar(16) NOT NULL,
+  `t13` varchar(16) NOT NULL,
+  `t14` varchar(16) NOT NULL,
+  `t15` varchar(16) NOT NULL,
+  `t16` varchar(16) NOT NULL,
+  `t17` varchar(16) NOT NULL,
+  `t18` varchar(16) NOT NULL,
+  `t19` varchar(16) NOT NULL,
+  `t20` varchar(16) NOT NULL,
+  `t21` varchar(16) NOT NULL,
+  `t22` varchar(16) NOT NULL,
+  `t23` varchar(16) NOT NULL,
+  `t24` varchar(16) NOT NULL,
+  `t25` varchar(16) NOT NULL,
+  `t26` varchar(16) NOT NULL,
+  `t27` varchar(16) NOT NULL,
+  `t28` varchar(16) NOT NULL,
+  `t29` varchar(16) NOT NULL,
+  `t30` varchar(16) NOT NULL,
+  `t31` varchar(16) NOT NULL,
+  `t32` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dentalchart`
+--
+
+INSERT INTO `dentalchart` (`id`, `patient_id`, `t1`, `t2`, `t3`, `t4`, `t5`, `t6`, `t7`, `t8`, `t9`, `t10`, `t11`, `t12`, `t13`, `t14`, `t15`, `t16`, `t17`, `t18`, `t19`, `t20`, `t21`, `t22`, `t23`, `t24`, `t25`, `t26`, `t27`, `t28`, `t29`, `t30`, `t31`, `t32`) VALUES
+(11, 22, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+(12, 23, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+(13, 24, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -24,19 +107,19 @@ SET time_zone = "+00:00";
 CREATE TABLE `medicines` (
   `id` int(11) NOT NULL,
   `medicine_name` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `medicines`
 --
 
 INSERT INTO `medicines` (`id`, `medicine_name`) VALUES
-(1, 'Amoxicillin'),
 (4, 'Antibiotic'),
 (5, 'Antihistamine'),
 (6, 'Atorvastatin'),
 (3, 'Losartan'),
 (2, 'Mefenamic'),
+(1, 'None'),
 (7, 'Oxymetazoline');
 
 -- --------------------------------------------------------
@@ -49,14 +132,14 @@ CREATE TABLE `medicine_details` (
   `id` int(11) NOT NULL,
   `medicine_id` int(11) NOT NULL,
   `packing` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `medicine_details`
 --
 
 INSERT INTO `medicine_details` (`id`, `medicine_id`, `packing`) VALUES
-(1, 1, '50'),
+(1, 1, '0'),
 (2, 4, '50'),
 (3, 5, '50'),
 (4, 6, '25'),
@@ -72,7 +155,7 @@ INSERT INTO `medicine_details` (`id`, `medicine_id`, `packing`) VALUES
 
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -90,14 +173,16 @@ CREATE TABLE `patients` (
   `date_of_birth` date NOT NULL,
   `phone_number` varchar(12) NOT NULL,
   `gender` varchar(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `patients`
 --
 
 INSERT INTO `patients` (`id`, `patient_name`, `address`, `cnic`, `date_of_birth`, `phone_number`, `gender`) VALUES
-(1, 'Mark Cooper', 'Sample Address 101 - Updated', '123654789', '1999-06-23', '091235649879', 'Male');
+(22, 'Juan Dela Cruz', 'Block 21 Lot 45', 'abrenica.y@gmail.', '2023-04-07', '09972157320', 'Male'),
+(23, 'Annie Batungbakal', 'Block 21 Lot 45', 'abrenica.y@gmail.', '2023-04-11', '09972157320', 'Female'),
+(24, 'Josh Pilay', 'Block 21 Lot 45', 'abrenica.y@gmail.', '2023-04-17', '09972157320', 'Male');
 
 -- --------------------------------------------------------
 
@@ -111,17 +196,15 @@ CREATE TABLE `patient_medication_history` (
   `medicine_details_id` int(11) NOT NULL,
   `quantity` tinyint(4) NOT NULL,
   `dosage` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `patient_medication_history`
 --
 
 INSERT INTO `patient_medication_history` (`id`, `patient_visit_id`, `medicine_details_id`, `quantity`, `dosage`) VALUES
-(1, 1, 1, 5, '250'),
-(2, 1, 6, 2, '500'),
-(3, 2, 2, 2, '250'),
-(4, 2, 7, 2, '250');
+(15, 14, 1, 12, '20'),
+(16, 15, 1, 0, '0');
 
 -- --------------------------------------------------------
 
@@ -133,19 +216,17 @@ CREATE TABLE `patient_visits` (
   `id` int(11) NOT NULL,
   `visit_date` date NOT NULL,
   `next_visit_date` date DEFAULT NULL,
-  `bp` varchar(23) NOT NULL,
-  `weight` varchar(12) NOT NULL,
-  `disease` varchar(30) NOT NULL,
+  `description` varchar(30) NOT NULL,
   `patient_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `patient_visits`
 --
 
-INSERT INTO `patient_visits` (`id`, `visit_date`, `next_visit_date`, `bp`, `weight`, `disease`, `patient_id`) VALUES
-(1, '2022-06-28', '2022-06-30', '120/80', '65 kg.', 'Wounded Arm', 1),
-(2, '2022-06-30', '2022-07-02', '120/80', '65 kg.', 'Rhinovirus', 1);
+INSERT INTO `patient_visits` (`id`, `visit_date`, `next_visit_date`, `description`, `patient_id`) VALUES
+(14, '2023-04-07', '2023-04-21', 'Checkup', 24),
+(15, '2023-04-11', '2023-04-18', 'Checkup Only', 22);
 
 -- --------------------------------------------------------
 
@@ -159,19 +240,31 @@ CREATE TABLE `users` (
   `user_name` varchar(30) NOT NULL,
   `password` varchar(100) NOT NULL,
   `profile_picture` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `display_name`, `user_name`, `password`, `profile_picture`) VALUES
-(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', '1656551981avatar.png '),
-(2, 'John Doe', 'jdoe', '9c86d448e84d4ba23eb089e0b5160207', '1656551999avatar_.png');
+(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', 'iconn.jpg'),
+(3, 'admin3', 'admin3', '202cb962ac59075b964b07152d234b70', '1680802143iconn.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dentalchart`
+--
+ALTER TABLE `dentalchart`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `medicines`
@@ -226,10 +319,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `dentalchart`
+--
+ALTER TABLE `dentalchart`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `medicines`
 --
 ALTER TABLE `medicines`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `medicine_details`
@@ -247,25 +352,25 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `patient_medication_history`
 --
 ALTER TABLE `patient_medication_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `patient_visits`
 --
 ALTER TABLE `patient_visits`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -290,3 +395,7 @@ ALTER TABLE `patient_medication_history`
 ALTER TABLE `patient_visits`
   ADD CONSTRAINT `fk_patients_visit_patient_id` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
